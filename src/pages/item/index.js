ebab8df82dca9a0cfaedda590e59698c86dfc2e6
rlruedas <reister.ruedas@gmail.com>
2022-08-12 17:06:16 +0800

    Minor Changes on Display

--- src/pages/item/index.js
+++ src/pages/item/index.js
@@ -30,8 +30,7 @@ function Item() {
       {loading ? (
         <Loader />
       ) : data ? (
-        <>
-          <section className="flex flex-col w-screen h-fit bg-black font-Poppins justify-center items-center overflow-x-clip ">
+          <section className="flex flex-col w-full min-h-fit bg-black font-Poppins justify-center items-center !overflow-x-clip ">
             <section className="w-[80vw] h-[5em] flex text-white justify-start items-center">
               <a
                 href={`javascript:history.go(-1)`}
@@ -113,9 +112,8 @@ function Item() {
                 <span className="opacity-50">{`${data?.story ?? ""} `}</span>
               </h3>
             </section>
+            <Footer />
           </section>
-          <Footer />
-        </>
       ) : (
         <div className="flex flex-col justify-center items-center w-screen h-screen overflow-hidden font-Poppins gap-5">
           <h1>Sorry, this Sneaker's Data is not yet on the Database. </h1>
