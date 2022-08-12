48892bbf069c9fb2e71b34ebb35e2ce7a54e8f43
rlruedas <reister.ruedas@gmail.com>
2022-08-12 17:18:24 +0800

    Re-add react strict mode for development

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./style/index.css";
 import App from "./App";
-import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
