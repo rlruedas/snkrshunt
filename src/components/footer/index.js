626f02f4630f0c3a5c16da2fd85a363344dfbc75
rlruedas <reister.ruedas@gmail.com>
2022-08-12 17:15:33 +0800

    Changes on display UI

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -3,8 +3,9 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className="flex justify-center items-center w-screen min-h-[50px] font-Poppins bg-[#DCBA62]">
-      <span className='font-medium text-[8px] sm:text-[15px]'> ©2022 SNRKSHunt All Rights Reserved | <a href="/">Home</a> | <a href="/about">About</a> </span>
+    <div className="flex flex-col justify-center items-center gap-2 w-full min-h-[6em] font-Poppins bg-[#DCBA62]">
+      <span><a href="/">Home</a> | <a href="/about">About</a> </span>
+      <span className='font-medium text-[8px] sm:text-[15px]'> ©2022 SNRKSHunt All Rights Reserved </span>
 
     </div>
   )
